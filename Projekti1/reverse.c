b5178c81438b9f70d7f6550f5a4e34b4da8275fd
Venla6nen <venla.kuusinen@gmail.com>
2026-02-07 13:04:40 +0200

    file reading and writing

--- Projekti1/reverse.c
+++ Projekti1/reverse.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]){
    
@@ -8,44 +9,118 @@ int main(int argc, char *argv[]){
         return(0);
     }
     if (argc ==2) {
-        char* pMemory = NULL;
-        int memorySize = 256;
-        
+
+        //source for pointers:https://www.geeksforgeeks.org/c/c-pointer-to-pointer-double-pointer/
+        char *pLine =NULL;
+        char **pReverseLines = NULL;
+        //source for getline():https://man7.org/linux/man-pages/man3/getline.3.html
+        size_t lineSize = 0;
+        __ssize_t read;
+
         int count =0;
+        int newMemorySize =0;
+        char **ptrNewMemorySize =NULL;
 
         FILE *inputfile;
-        inputfile =fopen(argv[1], "r");  
+        inputfile =fopen(argv[1], "r"); 
 
         if(inputfile ==NULL){
-            perror("Tiedoston avaaminen epäonnistui"); 
             exit(1);
         }
+
+        while ((read = getline(&pLine, &lineSize, inputfile)) != -1)
         
-        pMemory = (char*)malloc(memorySize);
+        {
+            char *plineCopy =malloc(read +1);
+            if (plineCopy == NULL) {
+                exit(1);
+            }
+            strcpy(plineCopy, pLine);
 
-        if ((pMemory = (char *)malloc(memorySize)) == NULL)
-            {perror("Muistinvaraus epäonnistui"); 
-            exit(1); 
+            //source for realloc():https://www.programiz.com/c-programming/c-dynamic-memory-allocation
+            if (count >= newMemorySize) {
+               ptrNewMemorySize = realloc(pReverseLines, (newMemorySize + 1) * sizeof(char*));
+                if(ptrNewMemorySize == NULL) {
+                    exit(1);
+                }
+                pReverseLines = ptrNewMemorySize;
+                newMemorySize++;
+            }
+            pReverseLines[count] = plineCopy;
+            count++;    
+            }
+
+            for(int i = count -1; i >= 0; i--){
+                printf("%s", pReverseLines[i]);
+                free(pReverseLines[i]);
+            }
+            free(pReverseLines);
+            free(pLine);
+            fclose(inputfile);
+            return(0);
         }
-        while(fgets(pMemory, memorySize, inputfile) != NULL){
-            
-        
-
-            printf("%s", pMemory);
-        }
-      
-
-        free(pMemory);
-        fclose(inputfile);
-        return(0);  
-
-    }
-    
     if (argc ==3){
-        return(0);
-    }
+        
+        //source for pointers:https://www.geeksforgeeks.org/c/c-pointer-to-pointer-double-pointer/
+        char *pLine =NULL;
+        char **pReverseLines = NULL;
+        //source for getline():https://man7.org/linux/man-pages/man3/getline.3.html
+        size_t lineSize = 0;
+        __ssize_t read;
+
+        int count =0;
+        int newMemorySize =0;
+        char **ptrNewMemorySize =NULL;
+
+        FILE *inputfile;
+        inputfile =fopen(argv[1], "r"); 
+
+        FILE *outputfile;
+        outputfile =fopen(argv[2], "w");
+
+        if(inputfile ==NULL){
+            exit(1);
+        }
+
+       
+
+        while ((read = getline(&pLine, &lineSize, inputfile)) != -1)
+        
+        {
+            char *plineCopy =malloc(read +1);
+            if (plineCopy == NULL) {
+                exit(1);
+            }
+            strcpy(plineCopy, pLine);
+
+            //source for realloc():https://www.programiz.com/c-programming/c-dynamic-memory-allocation
+            if (count >= newMemorySize) {
+               ptrNewMemorySize = realloc(pReverseLines, (newMemorySize + 1) * sizeof(char*));
+                if(ptrNewMemorySize == NULL) {
+                    exit(1);
+                }
+                pReverseLines = ptrNewMemorySize;
+                newMemorySize++;
+            }
+            pReverseLines[count] = plineCopy;
+            count++;    
+            }
+
+            for(int i = count -1; i >= 0; i--){
+
+                fprintf(outputfile, "%s", pReverseLines[i]);
+                free(pReverseLines[i]);
+            }
+            free(pReverseLines);
+            free(pLine);
+            fclose(inputfile);
+            fclose(outputfile);
+            return(0);
+        }
     else{
         return(0);
     }
+
 }
+
 
